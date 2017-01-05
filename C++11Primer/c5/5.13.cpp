e563ba1333c0d92a1e801e245d71efa499081260
Jayson Boubin <boubinjg@gmail.com>
2017-01-04 21:38:31 -0500

    5.3

---
+++ C++11Primer/c5/5.13.cpp
@@ -0,0 +1,7 @@
+a) the program falls through to all cases, because there are no break statments
+
+b) ix is assigned in one case and used in another. This is illegal
+
+c) you can't use the comma operator for case statements
+
+d) you can't use variables in case statements.
