ee43c22af4a7ab2e086546973423c22f4b07f430
Jayson Boubin <boubinjg@gmail.com>
2017-01-10 23:01:13 -0500

    7.5.5

---
+++ C++11Primer/c7/7.52.cpp
@@ -0,0 +1,8 @@
+There should not be a problem initializing the original version of Sales_data with those parameters
+given the original version looks like this:
+
+Sales_data {
+	std::string ISBN;
+	unsigned units_sold;
+	double revenue;
+};
