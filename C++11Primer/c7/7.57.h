adeb2a3694bba6e5008d3c3ae3eb40a8166cb9e9
Jayson Boubin <boubinjg@gmail.com>
2017-01-10 23:34:33 -0500

    chapt 7

---
+++ C++11Primer/c7/7.57.h
@@ -0,0 +1,9 @@
+class Account {
+public:
+	static double rate() { return interestRate; }
+	static void rate(double);
+private:
+	static constexpr int period = 30;
+	double daily_tbl[period];
+	static double interestRate;
+};
