5a930b8b175b92528e5cb3bfb5d577af80558cc3
Jayson Boubin <boubinjg@gmail.com>
2017-01-10 22:08:07 -0500

    7.51

---
+++ C++11Primer/c7/7.40.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+class Book 
+{
+	using page = std::vector<std::string>;
+	using pageVec = std::vector<page>;
+private:
+	pageVec pages;
+	std::string ISBN, title, author;
+public:
+	Book() = default;
+	Book(std::string is, std::string ti, std::string au) 
+	: ISBN(is), title(ti), author(au) {}
+	Book(std::string is, std::string ti, std::string au, pageVec pv) 
+	: ISBN(is), title(ti), author(au), pages(pv) {}
+
+};
