ec15ab01db2ab2f7002764b7529875909a8d10da
Jayson Boubin <boubinjg@gmail.com>
2017-01-11 22:47:54 -0500

    9.2.2

--- C++11Primer/c2/2.42.cpp
+++ C++11Primer/c2/2.42.cpp
@@ -6,7 +6,7 @@ int main()
 	//ex 1.20
 	std::cout<<"1.20"<<std::endl;
 	Sales_data s1, s2;
-	while(std::cin>>s2.bookNo>>s2.units_sold>>s2.revenue)
+	while(std::cin>>s1.bookNo>>s1.units_sold>>s1.revenue)
 	{
 		std::cout<<s1.bookNo<<" "<<s1.units_sold<<" "
 		         <<s1.revenue+s2.revenue;
