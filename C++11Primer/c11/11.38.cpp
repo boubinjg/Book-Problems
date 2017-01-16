63589db3b11c3332ac6e9cc1013dde018d1b7dee
Jayson Boubin <boubinjg@gmail.com>
2017-01-16 14:51:49 -0500

    chapt 11

---
+++ C++11Primer/c11/11.38.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<exception>
+const std::string &
+transform(const std::string &s, const std::unordered_map<std::string, std::string> &m)
+{
+	auto map_it = m.find(s);
+	if(map_it != m.cend())
+		return map_it->second;
+	else
+		return s;
+}
+std::unordered_map<std::string, std::string> buildMap(std::ifstream &map_file)
+{
+	std::unordered_map<std::string, std::string> trans_map;
+	std::string key, value;
+	while(map_file>>key && getline(map_file, value)) {
+		if(value.size() > 1)
+			trans_map[key] = value.substr(1);
+		else
+			throw "no rule for " + key;
+	}
+	return trans_map;
+}
+void word_transform(std::ifstream& map_file, std::ifstream& input)
+{
+	auto trans_map = buildMap(map_file);
+	std::string text;
+	while(getline(input, text)) {
+		std::istringstream stream(text);
+		std::string word;
+		bool firstword = true;
+		while(stream >> word) {
+			if(firstword)
+				firstword=false;
+			else
+				std::cout<<" ";
+			std::cout<<transform(word,trans_map);
+		}
+		std::cout<<std::endl;
+	}
+}
+int main()
+{
+	std::ifstream in("11.33.in"), m("11.33.map");
+	word_transform(m, in);
+	return 0;
+}
