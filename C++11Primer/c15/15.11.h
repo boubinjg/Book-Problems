297fab9449f1ba2ddc0e174ca778d62160aa788f
Jayson Boubin <boubinjg@gmail.com>
2017-01-27 12:42:20 -0500

    15.3

---
+++ C++11Primer/c15/15.11.h
@@ -0,0 +1,38 @@
+#include<string>
+#include<iostream>
+class Quote{
+public:
+	Quote() = default;
+	Quote(const std::string &book, double sales_price) :
+		bookNo(book), price(sales_price) {} 
+	std::string isbn() const {return bookNo;}
+	virtual double net_price(std::size_t n) const 
+		{return n * price;}
+	virtual void debug() const {std::cout<<"QUOTE: "<<bookNo<<" "<<price<<std::endl;}
+	virtual ~Quote() = default;
+private:
+	std::string bookNo;
+protected:
+	double price = 0.0;
+};
+
+class Bulk_quote : public Quote{
+public:	
+	Bulk_quote() = default;
+	Bulk_quote(const std::string&, double, std::size_t, double);
+	double net_price(std::size_t) const override;
+	void debug() const override {std::cout<<"BULK_QUOTE: "<<isbn()<<" "<<price
+			                      <<" "<<min_qty<<" "<<discount<<std::endl;} 
+private:
+	std::size_t min_qty = 0;
+	double discount = 0.0;
+};
+
+double print_total(std::ostream &os, const Quote &item, size_t n)
+{
+	double ret = item.net_price(n);
+	os << "ISBN: " << item.isbn() << " # sold: " << n 
+	<< " total due: " << ret << std::endl;
+
+	return ret;
+}
