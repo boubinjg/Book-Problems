5dfcbfbc3031ff8d04512ffb20df27e934e89451
Jayson Boubin <boubinjg@gmail.com>
2017-01-29 23:13:55 -0500

    15.9.3

--- C++11Primer/c15/15.35.h
+++ C++11Primer/c15/15.35.h
@@ -23,6 +23,65 @@ private:
 	Query(std::shared_ptr<Query_base> query) : q(query) {} 
 	std::shared_ptr<Query_base> q;
 };
+
+class WordQuery : public Query_base{
+	friend class Query;
+	WordQuery(const std::string &s) : query_word(s) {std::cout<<"WordQuery Constructor"<<std::endl;}
+	QueryResult eval(const TextQuery &t) const {return t.query(query_word); }
+	std::string rep() const {std::cout<<"WordQuery Rep"<<std::endl; return query_word;}
+	std::string query_word;
+};
+
+class NotQuery : public Query_base{
+	friend Query operator~(const Query &);
+	NotQuery(const Query &q) : query(q) {std::cout<<"NotQuery Constructor"<<std::endl;}
+	std::string rep() const {std::cout<<"NotQuery Rep"<<std::endl; return "~("+query.rep() + ")";}
+	QueryResult eval(const TextQuery&) const;
+	Query query;
+};
+
+class BinaryQuery : public Query_base{
+protected:
+	BinaryQuery(const Query &l, const Query &r, std::string s) :
+		lhs(l), rhs(r), opSym(s) {std::cout<<"BinQuery Constructor"<<std::endl;}
+	std::string rep() const {std::cout<<"BinQuery Rep"<<std::endl;
+			         return "("+rhs.rep()+" "+opSym+" "+lhs.rep()+")";}
+	Query lhs, rhs;
+	std::string opSym;
+};
+
+class AndQuery : public BinaryQuery{
+	friend Query operator&(const Query&, const Query&);
+	AndQuery(const Query &left, const Query &right) :
+		BinaryQuery(left, right, "&") {std::cout<<"AndQuery Constructor"<<std::endl;}
+	QueryResult eval(const TextQuery&) const;
+};
+
+class OrQuery : public BinaryQuery{
+	friend Query operator|(const Query&, const Query&);
+	OrQuery(const Query &left, const Query &right) :
+		BinaryQuery(left, right, "|") {std::cout<<"OrQuery Constructor"<<std::endl;}
+	QueryResult eval(const TextQuery&) const;
+};
+
+inline Query operator|(const Query &lhs, const Query &rhs)
+{
+	return std::shared_ptr<Query_base>(new OrQuery(lhs, rhs));
+}
+
+inline Query operator&(const Query &lhs, const Query &rhs)
+{
+	return std::shared_ptr<Query_base>(new AndQuery(lhs, rhs));
+}
+
+inline Query operator~(const Query &op)
+{
+	return std::shared_ptr<Query_base>(new NotQuery(op));
+}
+
+inline
+Query::Query(const std::string &s) : q(new WordQuery(s)) {std::cout<<"Query Constructor"<<std::endl;}
+
 std::ostream & operator<<(std::ostream &os, const Query &query)
 {
 	return os<<query.rep();
---
+++ C++11Primer/c15/15.37.cpp
@@ -0,0 +1,2 @@
+They would have to each reimplement basic features of the Query class. Query is meant to be an adapter for the
+Query_base class, as well as an umbrella class for organizing the derived classes.
