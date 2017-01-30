9baf40ca16ae77adabdfadd607ae8faf788e478d
Jayson Boubin <boubinjg@gmail.com>
2017-01-29 22:39:29 -0500

    15.9.2

--- C++11Primer/c15/15.35.h
+++ C++11Primer/c15/15.35.h
@@ -6,5 +6,24 @@ protected:
 	virtual ~Query_base() = default;
 private:	
 	virtual QueryResult eval(const TextQuery&) const = 0;
-	virtual std::string ret() const = 0;
+	virtual std::string rep() const = 0;
 };
+
+class Query{
+friend Query operator~(const Query &);
+friend Query operator|(const Query&, const Query&);
+friend Query operator&(const Query&, const Query&);
+
+public:
+	Query(const std::string&);
+	QueryResult eval(const TextQuery &t) const
+		{return q->eval(t);}
+	std::string rep() const { return q->rep();}
+private:
+	Query(std::shared_ptr<Query_base> query) : q(query) {} 
+	std::shared_ptr<Query_base> q;
+};
+std::ostream & operator<<(std::ostream &os, const Query &query)
+{
+	return os<<query.rep();
+}
