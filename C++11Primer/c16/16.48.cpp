da578fe4dbca0671c1aa8e2b292be4df534566bf
Jayson Boubin <boubinjg@gmail.com>
2017-02-03 13:50:16 -0500

    16.3

---
+++ C++11Primer/c16/16.48.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+template<typename T> std::string debug_rep(const T&);
+template<typename T> std::string debug_rep(T*);
+std::string debug_rep(const std::string &s);
+std::string debug_rep(char*);
+std::string debug_rep(const char*);
+
+template <typename T> std::string debug_rep(const T &t)
+{
+	std::ostringstream ret;
+	ret << t;
+	return ret.str();
+}
+template <typename T> std::string debug_rep(T *p)
+{
+	std::ostringstream ret;
+	ret << "pointer : " << p;
+	if(p)
+		ret <<" "<<debug_rep(*p);
+	else
+		ret << "null ptr";
+	return ret.str();
+}
+std::string debug_rep(const std::string &s)
+{
+	return '"' + s + '"';
+
+}
+std::string debug_rep(char* p)
+{
+	return debug_rep(std::string(p));
+}
+std::string debug_rep(const char *p)
+{
+	return debug_rep(std::string(p));
+}
+int main()
+{
+	return 0;
+}
