c756c2cf2f3def0501e77cd6870bb6d6af5d1927
Jayson Boubin <boubinjg@gmail.com>
2017-03-05 22:22:55 -0500

    11

--- EffectiveC++/11.cpp
+++ EffectiveC++/11.cpp
@@ -2,4 +2,35 @@
 
 //assignment to self happens often with aliasing.
 
+class Widget{
+	int *pb;
+	/* This version of the widget assignment operator is not safe,
+	 * if rhs and this are the same, we would delete pb, then use pb
+	Widget& operator=(const Widget& rhs) {
+		delete pb;
+		pb = new int(*rhs.pb);
+		return *this;
+	}*/
 
+	/* This version "works" but can still cause exceptions.
+	 * if the int construction throws an exception, pb will be undefined.
+	 * we can do better
+	Widget& operator=(const Widget& rhs) {
+		if(&rhs == this) return *this;
+		delete pb;
+		pb = new int(*rhs.pb);
+		return *this;
+	}*/
+	//Here, if int throws an exception, pb remains unchanged. 
+	Widget& operator=(const Widget& rhs) {
+		int *pOrig = pb;
+		pb = new int(*rhs.pb);
+		delete pOrig;
+		return *this;
+	}
+};
+
+int main()
+{
+	return 0;
+}
