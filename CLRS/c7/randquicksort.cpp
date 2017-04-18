d96f43a9650df959339f6538b2f197366f94b5c4
Jayson Boubin <boubinjg@gmail.com>
2017-04-17 21:38:54 -0400

    7.3

--- CLRS/c7/randquicksort.cpp
+++ CLRS/c7/randquicksort.cpp
@@ -1,5 +1,12 @@
 #include<vector>
 #include<iostream>
+#include<random>
+int rand(int beg, int end) 
+{
+	static std::default_random_engine e;
+	std::uniform_int_distribution<int> d(beg, end);
+	return d(e);
+}
 void print(std::vector<int>& v) 
 {
 	for(auto i : v)
@@ -21,10 +28,16 @@ int partition(std::vector<int>& v, int beg, int end)
 	std::swap(v[i],v[end]);
 	return i;
 }
+int randpartition(std::vector<int>& v, int beg, int end)
+{
+	int p = rand(beg, end);
+	std::swap(v[p], v[end]);
+	return partition(v, beg, end);
+}
 void quicksort(std::vector<int>& v, int beg, int end)
 {
 	if(beg < end) {
-		int p = partition(v, beg, end);
+		int p = randpartition(v, beg, end);
 		quicksort(v, beg, p-1);
 		quicksort(v, p+1, end);
 	}
